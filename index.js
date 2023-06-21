42a70e68d8d2897b5877946b973e7641f7ae5ff5
GREEDnim <sivadhas007@gmail.com>
2023-06-21 23:36:09 +0530

    socket io intial itertion implemented

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const http= require('http')
+const {Server} = require('socket.io')
+const cors=require('cors')
+
+
+const app=express()
+app.use(cors);
+const server=http.createServer(app);
+const io=new Server(server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods:['GET','POST'],
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log("user connected",socket.id);
+    socket.on('send-piece',(data)=>{
+        // console.log(data);
+        socket.broadcast.emit('receive-piece',data);
+    })
+})
+server.listen(3001,()=>{
+    console.log('server started at 3001')
+})
